73ddd3703efb55714a81547a90ac88c1341fdeb9
Alexander Kern <alexgit@alexkern.de>
2026-01-16 20:20:22 +0100

    rustRpnn8

--- src/main6.rs
+++ src/main6.rs
@@ -1,3 +1,4 @@
+use std::fs::File;                                                          use std::io::{self, BufRead};                                               use std::path::Path;
 use std::env;
 #[derive(Debug)]
 enum Element {
@@ -45,6 +46,11 @@ fn main() {
     let (dashes, params) = parse_cli_args(&args);
     // Oder: &args[..]
     // let (dashes, params) = parse_cli_args(&args[..]);
+    let pfad = "/data/data/com.termux/files/home/Eigene-Dateien/rpnn/csv/religion.csv";
+                                                                                match lese_csv_in_matrix(pfad) {
+        Ok(matrix) => {                                                                 println!("csv Datei erfolgreich eingelesen:");                                  //print_recursive(&matrix, 0);
+        }                                                                           Err(e) => eprintln!("Fehler beim Lesen der Datei: {}", e),
+    }
 
 }
 
@@ -294,4 +300,18 @@ fn parse_cli_args(args: &[String]) -> (Vec<usize>, Vec<String>) {
     }
     
     (minuses, params)
+}
+
+fn lese_csv_in_matrix(dateipfad: &str) -> io::Result<Element> {
+    let datei = File::open(dateipfad)?;
+    let reader = io::BufReader::new(datei);
+    let mut haupt_liste = Vec::new();                                       
+    for zeile_result in reader.lines() {                                            let zeile = zeile_result?;                                                  // Splitte die Zeile am Semikolon
+        let spalten: Vec<Element> = zeile                                               .split(';')
+            .map(|s| Element::Text(s.trim().to_string()))                               .collect();                                                     
+        // Füge die Zeile als Liste zur Hauptliste hinzu
+        haupt_liste.push(Element::Liste(spalten));
+    }
+
+    Ok(Element::Liste(haupt_liste))
 }
