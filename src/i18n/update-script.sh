abfb952f54f4ee58713a500246fdef00d028f802
Alexander Kern <alexgit@alexkern.de>
2026-03-25 16:43:54 +0100

    MuellGeloescht

--- src/i18n/update-script.sh
+++
@@ -1,5 +0,0 @@
-echo '' > messages.po # xgettext needs that file, and we need it empty
-find . -type f -iname "*.py" | xgettext -j -f - # this modifies messages.po
-msgmerge -N existing.po messages.po > new.po
-mv new.po existing.po
-rm messages.po
