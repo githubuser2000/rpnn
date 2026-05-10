4c5e13068617dccccd3fff5259fcddc3fcfdd853
Alexander Kern <alexgit@alexkern.de>
2026-05-10 16:07:19 +0200

    v0.6.4b

--- retaRust/termux_copy.sh
+++ retaRust/termux_copy.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-cp target/release/rreta ~/../usr/bin/
-cp target/release/rrp* ~/../usr/bin/
-cp target/release/*.so ~/../usr/lib/
+#!/bin/bash
+if [ "${1}" = "debug" ]
+then
+  cp target/debug/rreta ~/../usr/bin/
+  cp target/debug/rrp* ~/../usr/bin/
+  cp target/debug/*.so ~/../usr/lib/
+else
+  cp target/release/rreta ~/../usr/bin/
+  cp target/release/rrp* ~/../usr/bin/
+  cp target/release/*.so ~/../usr/lib/
+fi
